d53b984cc74c2c430407420a47d027c144e4f7ab
Nate Sales <nate@natesales.net>
2020-09-21 22:50:46 -0700

    agent: create content_directory variable

--- agent.py
+++ agent.py
@@ -2,14 +2,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+content_directory = "/tmp/bind/"
+
 
 @app.route("/refresh_zones", methods=["POST"])
 def refresh_zones():
-    with open("/etc/bind/named.conf.local", "w") as named_local_file:
+    with open(content_directory + "named.conf.local", "w") as named_local_file:
         named_local_file.write(request.json["payload"])
 
 
 @app.route("/refresh_single_zone", methods=["POST"])
 def refresh_single_zone():
-    with open("/etc/bind/db." + request.json["zone"], "w") as zone_file:
+    with open(content_directory + "db." + request.json["zone"], "w") as zone_file:
         zone_file.write(request.json["zone_file"])
