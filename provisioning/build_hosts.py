5b11eb170fb11bd2dfc3b5ecc3ce6e09033e7c1b
Nate Sales <nate@natesales.net>
2020-09-30 15:21:33 -0700

    provisioning: add build_hosts

---
+++ provisioning/build_hosts.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+import yaml
+
+db_client = MongoClient("mongodb://localhost:27017")
+
+with open("config.yml", "r") as config_file:
+    config = yaml.safe_load(config_file.read())
+
+_config = {
+    "all": {
+        "hosts": {},
+        "vars": {
+            "ansible_user": config["nodes"]["username"],
+            "ansible_port": config["nodes"]["port"],
+            "ansible_ssh_private_key_file": config["nodes"]["key"]
+        }
+    }
+}
+
+for node in db_client["cdn"]["nodes"].find():
+    _config["all"]["hosts"][node["name"]] = {
+        "ansible_host": node["management_ip"]
+    }
+
+with open("provisioning/hosts.yml", "w") as hosts_file:
+    hosts_file.write(yaml.dump(_config, default_flow_style=False))
