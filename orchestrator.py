1bf650bdc846aad2a732fe131917a2ee169b007e
Nate Sales <nate@natesales.net>
2020-09-29 22:57:20 -0700

    orchestrator: replace RESTful call with SSH transport

--- orchestrator.py
+++ orchestrator.py
@@ -8,8 +8,15 @@ from pymongo import MongoClient
 
 from config import configuration
 
+from paramiko import SSHClient, AutoAddPolicy
+from scp import SCPClient
+
 db_client = MongoClient("mongodb://localhost:27017")
 db = db_client["cdn"]
+
+ssh = SSHClient()
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(AutoAddPolicy())
 
 with open("templates/local.j2", "r") as local_template_file:
     # noinspection JinjaAutoinspect
@@ -49,10 +56,19 @@ def callback(ch, method, properties, body):
         for zone in db["zones"].find():
             zones_file += local_template.render(zone=zone["zone"])
 
+        with open("/tmp/named.conf.local", "w") as named_file:
+            named_file.write(zones_file)
+
         for node in db["nodes"].find():
-            print("Sending updated zone file to " + node["name"])
-            # TODO: Convert this to an SSH call
-            print(zones_file)
+            print("Sending updated zone file to " + node["name"] + " " + node["management_ip"])
+
+            ssh.connect(node["management_ip"], username="root", port=34553, key_filename="./ssh-key2")
+            with SCPClient(ssh.get_transport()) as scp:
+                scp.put("/tmp/named.conf.local", "zones.conf")
+            stdin, stdout, stderr = ssh.exec_command("rndc reload")
+            for line in stdout:
+                print('... ' + line.strip('\n'))
+            ssh.close()
 
 
 def main():
