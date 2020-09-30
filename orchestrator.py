426b47224598f6e705a54aa5a289dd4a873049c3
Nate Sales <nate@natesales.net>
2020-09-29 23:10:34 -0700

    orchestrator: cleanup comments

--- orchestrator.py
+++ orchestrator.py
@@ -19,11 +19,9 @@ ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(AutoAddPolicy())
 
 with open("templates/local.j2", "r") as local_template_file:
-    # noinspection JinjaAutoinspect
     local_template = Template(local_template_file.read())
 
 with open("templates/zone.j2") as zone_template_file:
-    # noinspection JinjaAutoinspect
     zone_template = Template(zone_template_file.read())
 
 
@@ -45,8 +43,7 @@ def callback(ch, method, properties, body):
             serial=zone["serial"]
         ))
 
-        # Pull data out of the database and assemble the zone file into an object
-        # POST the object to each node
+        # TODO: Pull data out of the database and assemble the zone file into a file and send it over
     elif operation == "refresh_zones":
         print("refreshing local zones file")
 
@@ -56,9 +53,11 @@ def callback(ch, method, properties, body):
         for zone in db["zones"].find():
             zones_file += local_template.render(zone=zone["zone"])
 
+        # Write the named.conf.local tmp file
         with open("/tmp/named.conf.local", "w") as named_file:
             named_file.write(zones_file)
 
+        # Loop over the nodes and send the updated zone file to each one, then reload the configuration
         for node in db["nodes"].find():
             print("... now updating " + node["name"] + " " + node["management_ip"] + " " + node["location"])
 
@@ -67,7 +66,7 @@ def callback(ch, method, properties, body):
             with SCPClient(ssh.get_transport()) as scp:
                 scp.put("/tmp/named.conf.local", "/etc/bind/named.conf.local")
 
-            print("    - reloading DNS config",end="", flush=True)
+            print("    - reloading DNS config", end="", flush=True)
             stdin, stdout, stderr = ssh.exec_command("rndc reload")
             for line in stdout:
                 print(" - " + line.strip('\n'))
@@ -82,7 +81,7 @@ def main():
     channel = connection.channel()
 
     channel.queue_declare(queue="cdn_updates")
-    channel.basic_consume(queue="cdn_updates", on_message_callback=callback, auto_ack=True)  # TODO: what is auto ack?
+    channel.basic_consume(queue="cdn_updates", on_message_callback=callback, auto_ack=True)
 
     print("Waiting for messages.")
     channel.start_consuming()
@@ -92,7 +91,7 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
