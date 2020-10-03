98dba67e410f0f10959d6f256bc7c20ca30c7354
Nate Sales <nate@natesales.net>
2020-10-03 00:24:46 -0700

    add MX and TXT records

--- src/components/RecordTable.svelte
+++ src/components/RecordTable.svelte
@@ -78,6 +78,8 @@
                     <Dropdown id="add-type" bind:content={type}>
                         <option value="A">A</option>
                         <option value="AAAA">AAAA</option>
+                        <option value="MX">MX</option>
+                        <option value="TXT">TXT</option>
                     </Dropdown>
                 </div>
 
