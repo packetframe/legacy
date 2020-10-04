7e0f022a304976d4a715e0a9c39f85447d472826
Nate Sales <nate@natesales.net>
2020-10-03 20:51:58 -0700

    create NumberInput component

---
+++ src/components/NumberInput.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let name, placeholder = "";
+    export let id;
+    export let content = "";
+</script>
+
+<main>
+    <label>
+        <input bind:value={content} id={id} name={name} placeholder={placeholder} type="number">
+    </label>
+</main>
+
+<style>
+    input {
+        border: 1px solid white;
+        border-radius: 5px;
+        margin: 2px;
+        padding: 10px;
+        background: transparent;
+        color: white;
+        width: calc(100% - 200px);
+    }
+</style>
