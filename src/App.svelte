d99af80b0e350287943bdb7f62935d9134d4b28d
Nate Sales <nate@natesales.net>
2020-10-05 22:51:12 -0700

    create login form

--- src/App.svelte
+++ src/App.svelte
@@ -5,26 +5,31 @@
     import {onMount} from "svelte";
     import NetworkMap from "./components/NetworkMap.svelte";
     import Button from "./components/Button.svelte";
+    import LoginForm from "./components/LoginForm.svelte";
 
     let zones;
     let selected_zone = window.location.toString().split("zone=")[1];
     let no_zones = false;
     let showMap = false;
 
-    onMount(() => {
-        fetch("http://localhost/api/zones/list")
-            .then(response => response.json())
-            .then(data => {
-                if (data["message"].length > 0) {
-                    zones = data["message"];
+    let loggedIn = false;
 
-                    if (selected_zone === undefined) {
-                        selected_zone = data["message"][0]["zone"];
+    onMount(() => {
+        if (loggedIn) {
+            fetch("http://localhost/api/zones/list")
+                .then(response => response.json())
+                .then(data => {
+                    if (data["message"].length > 0) {
+                        zones = data["message"];
+
+                        if (selected_zone === undefined) {
+                            selected_zone = data["message"][0]["zone"];
+                        }
+                    } else {
+                        no_zones = true;
                     }
-                } else {
-                    no_zones = true;
-                }
-            });
+                });
+        }
     });
 </script>
 
@@ -35,16 +40,32 @@
     </Navbar>
 
     <div class="body">
-        <div class="header-container">
-            <h1 class="header-text">CDN Dashboard</h1>
+        {#if loggedIn}
+            <div class="header-container">
+                <h1 class="header-text">CDN Dashboard</h1>
 
-            <Button onclick={() => showMap = !showMap} padded={true}>Toggle Map</Button>
-            {#if zones}
-                <Dropdown width="100%" bind:content={selected_zone}>
-                    {#each zones as zone}
-                        <option value="{zone['zone']}">{zone['zone']}</option>
-                    {/each}
-                </Dropdown>
+                <Button onclick={() => showMap = !showMap} padded={true}>Toggle Map</Button>
+                {#if zones}
+                    <Dropdown width="100%" bind:content={selected_zone}>
+                        {#each zones as zone}
+                            <option value="{zone['zone']}">{zone['zone']}</option>
+                        {/each}
+                    </Dropdown>
+                {:else}
+                    {#if no_zones}
+                        <p style="padding-left: 10px">No zones</p>
+                    {:else}
+                        <p style="padding-left: 10px">Loading...</p>
+                    {/if}
+                {/if}
+            </div>
+
+            {#if showMap}
+                <NetworkMap/>
+            {/if}
+
+            {#if selected_zone !== ""}
+                <RecordTable zone={selected_zone}/>
             {:else}
                 {#if no_zones}
                     <p style="padding-left: 10px">No zones</p>
@@ -52,20 +73,8 @@
                     <p style="padding-left: 10px">Loading...</p>
                 {/if}
             {/if}
-        </div>
-
-        {#if showMap}
-            <NetworkMap/>
-        {/if}
-
-        {#if selected_zone !== ""}
-            <RecordTable zone={selected_zone}/>
         {:else}
-            {#if no_zones}
-                <p style="padding-left: 10px">No zones</p>
-            {:else}
-                <p style="padding-left: 10px">Loading...</p>
-            {/if}
+            <LoginForm/>
         {/if}
     </div>
 
---
+++ src/components/LoginForm.svelte
@@ -0,0 +1,49 @@
+<script>
+    import Button from "./Button.svelte";
+    import TextInput from "./TextInput.svelte";
+
+    let username, password;
+</script>
+
+<main>
+    <div class="title">Login</div>
+    <div class="container">
+        <div class="form-element">
+            <TextInput placeholder="Username"/>
+        </div>
+
+        <div class="form-element">
+            <TextInput placeholder="Password"/>
+        </div>
+
+        <div class="form-element">
+            <Button inverted centered>Login</Button>
+        </div>
+    </div>
+</main>
+
+<style>
+    main {
+        border: 2px solid white;
+        border-radius: 15px;
+        width: 450px;
+        margin: auto;
+        flex-direction: column;
+    }
+
+    .title {
+        font-size: 20px;
+        text-align: center;
+        margin-top: 25px;
+    }
+
+    .container {
+        color: #ffff;
+        padding: 25px 25px 0;
+        border-radius: 3px;
+    }
+
+    .form-element {
+        padding-bottom: 20px;
+    }
+</style>
