748c6a5f2539c51c45b3f6a54625d0d520d16a93
Nate Sales <nate@natesales.net>
2020-10-08 21:46:04 -0700

    fix broken snackbar reactivity

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,5 @@ import {SnackBars} from "./stores";
 
 export function addSnackbar(status, message, color, timeout) {
     let id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    // $SnackBars[id] = {status, message, color, timeout}
-    alert(status + " " + message);
+    SnackBars.update(sb => {sb[id] = {status, message, color, timeout, done}; return sb})
 }
