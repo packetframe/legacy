c7d07b05872bcf620b03998b898f4d1c3d291a72
Nate Sales <nate@natesales.net>
2020-10-08 20:40:10 -0700

    fix missing import

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import {SnackBars} from "./stores";
+
 export function addSnackbar(status, message, color, timeout) {
     let id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
     $SnackBars[id] = {status, message, color, timeout}
