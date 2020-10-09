dbfa4b1b4d2eb9f2149909d21de63f3f7b265018
Nate Sales <nate@natesales.net>
2020-10-08 20:46:22 -0700

    util: rename ssh key

--- cdnctl.sh
+++ cdnctl.sh
@@ -11,7 +11,7 @@ if [[ "$1" == "stop" ]]; then
   echo -n "Stopping "
   mongo --quiet --eval 'db.nodes.find().forEach(function(node) {print(node.name, node.location);});' cdn | grep "$2"
   node_ip=$(mongo --quiet --eval 'db.nodes.find({"name": "'$2'"}).forEach(function(x) {print(x.management_ip);});' cdn)
-  ssh -p 34553 -i ssh-key2 root@"$node_ip" 'systemctl stop bird && echo "$(hostname) Stopped BGP process" || echo "ERROR: Failed to stop BGP process"'
+  ssh -p 34553 -i ssh-key root@"$node_ip" 'systemctl stop bird && echo "$(hostname) Stopped BGP process" || echo "ERROR: Failed to stop BGP process"'
   exit
 fi
 
@@ -19,7 +19,7 @@ if [[ "$1" == "start" ]]; then
   echo -n "Starting "
   mongo --quiet --eval 'db.nodes.find().forEach(function(node) {print(node.name, node.location);});' cdn | grep "$2"
   node_ip=$(mongo --quiet --eval 'db.nodes.find({"name": "'$2'"}).forEach(function(x) {print(x.management_ip);});' cdn)
-  ssh -p 34553 -i ssh-key2 root@"$node_ip" 'systemctl start bird && echo "$(hostname) Started BGP process" || echo "ERROR: Failed to start BGP process"'
+  ssh -p 34553 -i ssh-key root@"$node_ip" 'systemctl start bird && echo "$(hostname) Started BGP process" || echo "ERROR: Failed to start BGP process"'
   exit
 fi
 
@@ -29,4 +29,4 @@ echo -n Jumping to $node
 node_ip=$(mongo --quiet --eval "db.nodes.findOne({name: \"${node}\"})[\"management_ip\"]" cdn)
 echo " - $node_ip"
 
-ssh -i ssh-key2 -p 34553 root@$node_ip
+ssh -i ssh-key -p 34553 root@$node_ip
