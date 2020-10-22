446bc07e8a48fe21ffd8aa1a1d88898187ab179f
Nate Sales <nate@natesales.net>
2020-10-21 23:00:16 -0700

    add pinned message

--- src/components/RecordTable.svelte
+++ src/components/RecordTable.svelte
@@ -231,6 +231,7 @@
 
                 {#if showNodePinning }
                     <div class="info-text">
+                        <p>This record <u>will</u> be pinned to the following nodes:</p>
                         <div class="node-pinning-checkboxes">
                             {#if nodes}
                                 {#each nodes as node }
