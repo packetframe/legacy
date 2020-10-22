336a5ce55cc86a82a0fb75948b0a9e04a9c77b9e
Nate Sales <nate@natesales.net>
2020-10-22 14:29:26 -0700

    add blog post and remove extra index content

---
+++ blog/2020-10-22-hello-world.md
@@ -0,0 +1,13 @@
+---
+slug: hello-world
+title: Hello World
+author: Nate Sales
+author_title: delivr.dev
+author_url: https://natesales.net/
+author_image_url: https://avatars2.githubusercontent.com/u/20391440
+tags: ["Hello World"]
+---
+
+Hello World!
+
+Welcome to delivr.dev, the open source CDN platform for technology enthusiasts. delivr.dev is currently in private beta, so stay tuned for more information!
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,91 +7,93 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
 const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    // {
+    //     title: 'Easy to Use',
+    //     imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    //     description: (
+    //         <>
+    //             Docusaurus was designed from the ground up to be easily installed and
+    //             used to get your website up and running quickly.
+    //         </>
+    //     ),
+    // },
+    // {
+    //     title: 'Simple and Automatable',
+    //     imageUrl: 'img/undraw_docusaurus_tree.svg',
+    //     description: (
+    //         <>
+    //             delivr.dev can be controlled with the dashboard or API.
+    //         </>
+    //     ),
+    // },
+    // {
+    //     title: 'Fully Open Source',
+    //     imageUrl: 'img/undraw_docusaurus_react.svg',
+    //     description: (
+    //         <>
+    //             delivr.dev is fully open source, including third party components such as Caddy, Varnish, and BIRD.
+    //         </>
+    //     ),
+    // },
 ];
 
 function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+    const imgUrl = useBaseUrl(imageUrl);
+    return (
+        <div className={clsx('col col--4', styles.feature)}>
+            {imgUrl && (
+                <div className="text--center">
+                    <img className={styles.featureImage} src={imgUrl} alt={title}/>
+                </div>
+            )}
+            <h3>{title}</h3>
+            <p>{description}</p>
         </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
+    );
 }
 
 function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('https://dash.delivr.dev/')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
-    </Layout>
-  );
+    const context = useDocusaurusContext();
+    const {siteConfig = {}} = context;
+    return (
+        <Layout
+            title={`${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />">
+            <header>
+                <br/>
+                {/*TODO: Center align this*/}
+                <div>
+                    <div className="container">
+                        <h1 className="hero__title">{siteConfig.title}</h1>
+                        <p className="hero__subtitle">{siteConfig.tagline}</p>
+                        <div className={styles.buttons}>
+                            <Link
+                                className={clsx(
+                                    'button button--outline button--secondary button--lg',
+                                    styles.getStarted,
+                                )}
+                                to={useBaseUrl('https://dash.delivr.dev/')}>
+                                Get Started
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <main>
+                {features && features.length > 0 && (
+                    <section className={styles.features}>
+                        <div className="container">
+                            <div className="row">
+                                {features.map((props, idx) => (
+                                    <Feature key={idx} {...props} />
+                                ))}
+                            </div>
+                        </div>
+                    </section>
+                )}
+            </main>
+        </Layout>
+    );
 }
 
 export default Home;
