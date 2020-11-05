6ef8264a88b323ac828a30bbaa30f46d583104dd
Nate Sales <nate@natesales.net>
2020-11-04 17:50:22 -0800

    util: fix backup file

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 BACKUP_FILE="/var/log/delivr-backup.txt"
 
-echo "Backup at $(date)" > backup.txt
+echo "Backup at $(date)" > $BACKUP_FILE
 mongo --quiet --eval 'db.cache_nodes.find();' cdn >> $BACKUP_FILE
 mongo --quiet --eval 'db.nodes.find();' cdn >> $BACKUP_FILE
 mongo --quiet --eval 'db.zones.find();' cdn >> $BACKUP_FILE
