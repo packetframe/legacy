54d3d6310e5a67d2afe613a98b4142856090bac2
Nate Sales <nate@natesales.net>
2020-11-14 00:20:45 -0800

    docs: add CLI

---
+++ docs/docs/cli.md
@@ -0,0 +1,8 @@
+---
+id: cli
+title: CLI
+---
+
+For users that prefer a CLI option, there's a `delivrctl` tool that can wraps the API in a simple command line interface.
+
+The latest version of `delivrctl` can be found on GitHub (https://raw.githubusercontent.com/natesales/delivr/main/delivrctl.py)
--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,7 +2,8 @@ module.exports = {
     someSidebar: [
         {type:"category", label:"Dashboard", "items": ["record-management", "node-pinning", "caching-proxy", "adding-zones"]},
         {type: "doc", id: "api"},
-        {type: "doc", id: "privacy-policy"},
+        {type: "cli", id: "cli"},
         {type: "doc", id: "eca"},
+        {type: "doc", id: "privacy-policy"},
     ]
 };
