6f6c545988513fb63d30192030f75908d60b1a46
Nate Sales <nate@natesales.net>
2020-11-14 00:22:57 -0800

    docs: fix sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
     someSidebar: [
         {type:"category", label:"Dashboard", "items": ["record-management", "node-pinning", "caching-proxy", "adding-zones"]},
         {type: "doc", id: "api"},
-        {type: "cli", id: "cli"},
+        {type: "doc", id: "cli"},
         {type: "doc", id: "eca"},
         {type: "doc", id: "privacy-policy"},
     ]
