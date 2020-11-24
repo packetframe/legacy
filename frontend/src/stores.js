d9fa23d91734287edfe861ea2cbb3a7a4a33fb7c
Nate Sales <nate@natesales.net>
2020-11-24 13:02:46 -0800

    frontend: fix exported const

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,5 +1,6 @@
 import {writable} from "svelte/store";
 
 export const SnackBars = writable({});
-export let Page = writable("index");
 export let IsAdmin = writable(false);
+export let Debug = writable(true);
+export let API = writable("https://dash.delivr.dev/api/");
