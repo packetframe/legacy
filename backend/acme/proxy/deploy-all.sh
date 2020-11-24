d674eba4ca6a6da71f0468f76dd46af95b87834e
Nate Sales <nate@natesales.net>
2020-11-24 01:20:43 -0800

    add todo

--- backend/acme/proxy/deploy-all.sh
+++ backend/acme/proxy/deploy-all.sh
@@ -7,5 +7,6 @@ tar -czvf certs.tar.gz /etc/letsencrypt/live/*
 for node in $nodes ; do
 	echo Copying to $node
 	scp -P 34553 -i /home/nate/ssh-key certs.tar.gz root@$node:/etc/caddy/certs.tar.gz
-	ssh -p 34553 -i /home/nate/ssh-key root@$node "tar -xvzf /etc/caddy/certs.tar.gz ; chown -R caddy:caddy /etc/caddy/*.pem ; caddy reload -config /etc/caddy/Caddyfile"
+	ssh -p 34553 -i /home/nate/ssh-key root@$node "tar -xvzf /etc/caddy/certs.tar.gz -C /etc/caddy ; chown -R caddy:caddy /etc/caddy/*.pem ; caddy reload -config /etc/caddy/Caddyfile"
+#	TODO: This doesnt extract in the right place
 done
