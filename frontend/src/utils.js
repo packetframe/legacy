a674df5bc3bfc8393cf6aaaa0b2d087afa418bdc
Nate Sales <nate@natesales.net>
2020-11-24 01:09:46 -0800

    frontend: add debug function

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,12 @@
-import {SnackBars} from "./stores";
+import {SnackBars, Debug} from "./stores";
 
 export function addSnackbar(status, message, color, timeout) {
     let id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
     SnackBars.update(sb => {sb[id] = {status, message, color, timeout}; return sb})
+}
+
+export function log(message) {
+    if (Debug) {
+        console.log(message)
+    }
 }
