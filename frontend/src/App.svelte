0e952f6903d13bd5729cf79e53c740e513b97be6
Nate Sales <nate@natesales.net>
2020-11-27 01:07:04 -0800

    add peering page

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,6 +6,7 @@
     import Signup from "./pages/Signup.svelte";
     import Dashboard from "./pages/Dashboard.svelte";
     import Docs from "./pages/Docs.svelte";
+    import Peering from "./pages/Peering.svelte";
     import NotFound from "./pages/NotFound.svelte";
     import SnackbarGroup from "./components/SnackbarGroup.svelte";
 
@@ -14,9 +15,15 @@
         "/login": Login,
         "/signup": Signup,
         "/dashboard": Dashboard,
+        "/peering": Peering,
         "/docs": Docs,
         "*": NotFound
     }
+
+    // // Load global theme
+    // for (const [key, value] of Object.entries(DarkTheme)) {
+    //     document.documentElement.style.setProperty("--" + key, value)
+    // }
 </script>
 
 <main>
@@ -27,7 +34,7 @@
         <SnackbarGroup/>
     </div>
 
-    <footer>&copy; Nate Sales 2020.</footer>
+    <footer>&copy; PacketFrame 2020.</footer>
 </main>
 
 <style>
---
+++ frontend/src/pages/Peering.svelte
@@ -0,0 +1,16 @@
+<main>
+    <h1>Peering Policy</h1>
+
+    <p>PacketFrame has a generally open peering policy with networks that have a significant amount of traffic to the CDN.</p>
+</main>
+
+<style>
+    main {
+        width: 50%;
+        margin: auto;
+    }
+
+    h1 {
+        text-align: center;
+    }
+</style>
