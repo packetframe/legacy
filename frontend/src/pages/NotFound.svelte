8c6d8f8f37a9713bb06d2c4332cf9f40f21ab334
Nate Sales <nate@natesales.net>
2020-11-27 21:33:05 -0800

    condense 404 pages into one

--- frontend/src/pages/NotFound.svelte
+++ frontend/src/pages/NotFound.svelte
@@ -1,9 +1,69 @@
 <main>
-    <h1>404 Not Found</h1>
+    <h1>Error 404</h1>
+    <h2>Not Found</h2>
+    <div class="container">
+        <div class="figure-element">
+            <img alt="User" class="image" src="/static/img/laptop.png">
+            <div class="text-container">
+                <p>Browser</p>
+                <img alt="" class="icon" src="/static/img/cross.png">
+            </div>
+        </div>
+
+        <div class="figure-element">
+            <img alt="delivr.dev" class="image" src="/static/img/icon.png">
+            <div class="text-container">
+                <p>PacketFrame</p>
+                <img alt="" class="icon" src="/static/img/check.png">
+            </div>
+        </div>
+    </div>
 </main>
 
 <style>
-    main {
+    .container {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        flex-wrap: wrap;
+        width: 30%;
+        margin: auto;
+    }
+
+    h1 {
         text-align: center;
+        font-size: 3em;
+    }
+
+    h2 {
+        text-align: center;
+    }
+
+    .figure-element {
+        text-align: center;
+        margin-top: 15px;
+        font-size: 1.25em;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex-grow: 1;
+    }
+
+    .figure-element .image {
+        width: 200px;
+    }
+
+    .icon {
+        height: 50px;
+        width: 50px;
+    }
+
+    .text-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex-grow: 1;
     }
 </style>
