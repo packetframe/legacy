34cc0a329ffab08fc96dcca99929a0f4799783a2
Nate Sales <nate@natesales.net>
2020-12-07 15:30:23 -0800

    add network page

---
+++ frontend/src/pages/Network.svelte
@@ -0,0 +1,63 @@
+<script>
+    let globe = new ENCOM.Globe(window.innerWidth, window.innerHeight - (document.getElementsByTagName("main")[0].clientTop + document.getElementsByTagName("main")[0].clientHeight), {
+        font: "sans-serif",
+        data: [],
+        tiles: grid.tiles,
+        baseColor: "#dd00ff",
+        markerColor: "#8e44ad",
+        pinColor: "#aacfd1",
+        satelliteColor: "#aacfd1",
+        scale: 1,
+        dayLength: 14000,
+        introLinesDuration: 2000,
+        maxPins: 100,
+        maxMarkers: 100,
+        viewAngle: 0.75
+    });
+
+    document.getElementsByTagName("main")[0].appendChild(globe.domElement);
+
+    function animate() {
+        if (globe) globe.tick();
+        requestAnimationFrame(animate);
+    }
+
+    function initGlobe() {
+        globe.init();
+        animate();
+
+        fetch("https://packetframe.com/api/nodes/geoloc")
+            .then(response => response.json())
+            .then(data => {
+                for (const name in data["message"]) {
+                    const lat = parseFloat(data["message"][name].split(", ")[0]);
+                    const lon = parseFloat(data["message"][name].split(", ")[1]);
+                    globe.addPin(lat, lon, "");
+                    globe.addMarker(lat, lon, name, false);
+//                    globe.addSatellite(lat, lon, 0, {coreColor: "#ffffff", numWaves: 3});
+                    console.log("Added " + name + " " + lat + ", " + lon);
+                }
+            })
+    }
+
+    window.addEventListener('resize', () => {
+        let height = window.innerHeight - (document.getElementsByTagName("main")[0].clientTop + document.getElementsByTagName("main")[0].clientHeight);
+        globe.camera.aspect = window.innerWidth / height;
+        globe.camera.updateProjectionMatrix();
+        globe.renderer.setSize(window.innerWidth, height);
+    });
+
+    setTimeout(() => {
+        initGlobe();
+    }, 100);
+</script>
+
+
+<style>
+    main {
+        margin: 0;
+    }
+</style>
+
+
+<main></main>
