0bf711ec68c41559a717f6dd0c5d287b7b1e000e
Nate Sales <nate@natesales.net>
2020-12-10 20:28:03 -0800

    add node name to cdnctl

--- backend/cdnctl.sh
+++ backend/cdnctl.sh
@@ -4,6 +4,6 @@ SSH_KEY="/home/nate/ssh-key"
 
 echo -n Jumping to node $1
 node_ip=$(mongo --quiet --eval "db.nodes.findOne({name: \"${1}\"})[\"management_ip\"]" cdn)
-echo " - $node_ip"
+echo " - $node_ip" $(mongo --quiet --eval "db.nodes.findOne({name: \"${1}\"})[\"provider\"]" cdn)
 
 ssh -i $SSH_KEY -p 34553 root@$node_ip
