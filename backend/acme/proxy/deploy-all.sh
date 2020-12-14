231252c052f6afed0c63c4673f5cb4fc93ed0981
Nate Sales <nate@natesales.net>
2020-12-13 16:20:42 -0800

    fix cert deployment scripts

--- backend/acme/proxy/deploy-all.sh
+++ backend/acme/proxy/deploy-all.sh
@@ -2,11 +2,12 @@
 
 nodes=$(mongo --quiet --eval 'db.nodes.find({"http": true}).forEach(function(node) {print(node.management_ip);});' cdn)
 
-tar -czvf certs.tar.gz /etc/letsencrypt/live/*
-
 for node in $nodes ; do
-	echo Copying to $node
-	scp -P 34553 -i /home/nate/ssh-key certs.tar.gz root@$node:/etc/caddy/certs.tar.gz
-	ssh -p 34553 -i /home/nate/ssh-key root@$node "tar -xvzf /etc/caddy/certs.tar.gz -C /etc/caddy ; chown -R caddy:caddy /etc/caddy/*.pem ; caddy reload -config /etc/caddy/Caddyfile"
-#	TODO: This doesnt extract in the right place
+  echo "Updating $node"
+  for zone in $(ls /etc/letsencrypt/live/); do
+    scp -P 34553 -i /home/nate/ssh-key /etc/letsencrypt/live/$zone/privkey.pem root@$node:/etc/caddy/$zone-key.pem
+    scp -P 34553 -i /home/nate/ssh-key /etc/letsencrypt/live/$zone/fullchain.pem root@$node:/etc/caddy/$zone-chain.pem
+  done
+
+  ssh -p 34553 -i /home/nate/ssh-key root@$node "chown -R caddy:caddy /etc/caddy/*.pem ; caddy reload -config /etc/caddy/Caddyfile"
 done
