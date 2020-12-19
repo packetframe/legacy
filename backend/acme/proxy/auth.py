7cd6fb80641e80599614ee31f09087b18bd199de
Nate Sales <nate@natesales.net>
2020-12-19 15:51:12 -0800

    simplify proxy auth script

--- backend/acme/proxy/auth.py
+++ backend/acme/proxy/auth.py
@@ -2,19 +2,12 @@
 
 import os
 import requests
-import pymongo
 import time
 
 API_KEY = "ADMIN_API_KEY"
-zone = os.environ["CERTBOT_DOMAIN"]
-cdn_zone = ""
-true_zone = ".".join(zone.split(".")[1:])
 
-zones = pymongo.MongoClient("mongodb://localhost:27017")["cdn"]["zones"]
-for _zone in zones.find():
-    if _zone["zone"].endswith(true_zone) or _zone["zone"] == zone:
-        cdn_zone = _zone["zone"]
-        break
+zone = ""
+cdn_zone = ""
 
 r = requests.post("https://packetframe.com/api/zone/" + cdn_zone + "/add", headers={"X-API-Key": API_KEY}, json={
     "type": "TXT",
