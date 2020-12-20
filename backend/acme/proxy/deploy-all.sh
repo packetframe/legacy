22da2d0faee677fc6279769a94a83330e9de1f71
Nate Sales <nate@natesales.net>
2020-12-19 17:33:25 -0800

    add cert update script

--- backend/acme/proxy/deploy-all.sh
+++ backend/acme/proxy/deploy-all.sh
@@ -2,12 +2,18 @@
 
 nodes=$(mongo --quiet --eval 'db.nodes.find({"http": true}).forEach(function(node) {print(node.management_ip);});' cdn)
 
+rm -rf /tmp/certs/ certs.tar.gz
+mkdir /tmp/certs/
+
+for zone in $(ls /etc/letsencrypt/live/); do
+  cp /etc/letsencrypt/live/$zone/privkey.pem /tmp/certs/$zone-key.pem
+  cp /etc/letsencrypt/live/$zone/fullchain.pem /tmp/certs/$zone-chain.pem
+done
+
+tar -cvzf certs.tar.gz /tmp/certs/*
+
 for node in $nodes ; do
   echo "Updating $node"
-  for zone in $(ls /etc/letsencrypt/live/); do
-    scp -P 34553 -i /home/nate/ssh-key /etc/letsencrypt/live/$zone/privkey.pem root@$node:/etc/caddy/$zone-key.pem
-    scp -P 34553 -i /home/nate/ssh-key /etc/letsencrypt/live/$zone/fullchain.pem root@$node:/etc/caddy/$zone-chain.pem
-  done
-
-  ssh -p 34553 -i /home/nate/ssh-key root@$node "chown -R caddy:caddy /etc/caddy/*.pem ; caddy reload -config /etc/caddy/Caddyfile"
+  scp -P 34553 -i /home/nate/ssh-key certs.tar.gz root@$node:/etc/caddy/certs.tar.gz
+  ssh -p 34553 -i /home/nate/ssh-key root@$node "bash /root/update-certs.sh"
 done
